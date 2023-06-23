4ac0146a21e8071e798df6c71add1227208f30b7
Manish Chauhan <manish.fastprep@gmail.com>
2023-06-23 11:14:23 +0530

    initial commit;

---
+++ src/main/java/in/ushatech/az204/Az204Application.java
@@ -0,0 +1,13 @@
+package in.ushatech.az204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Az204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Az204Application.class, args);
+    }
+
+}
---
+++ src/test/java/in/ushatech/az204/Az204ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.az204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Az204ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
